a8e3fc12559bc53d3900b14a5ae7d5ffd429d5ac
iWisp360 <iwisp360@protonmail.com>
2024-01-24 10:24:18 -0500

    Code Optimization and DUCT now accept float values

--- Conversion Tool/formulas.py
+++ Conversion Tool/formulas.py
@@ -1,179 +1,185 @@
 def b2kb():
-    b = input("Express a number in Bytes: ")
-    if not b.isdigit():
-        print ("Only integers are allowed")
+    try:
+        b = input("Express a number in Bytes: ")  
+        kb = (int(b) / 1024)
+        print("\n" + str(b) + " bytes are " + str(kb) + " Kb :)")
+    except ValueError:
+        print ("When you type bytes, only integers are allowed")
         exit(1)
-    kb = (int(b) / 1024)
-
-    print("\n" + str(b) + " bytes are " + str(kb) + " Kb :)")
 
 def b2mb():
-    b = input("Express a number in Bytes: ")
-    if not b.isdigit():
-        print ("Only integers are allowed")
+    try:
+        b = input("Express a number in Bytes: ")
+        mb = (int(b) / 1024 ** 2)
+        print("\n" + str(b) + " bytes are " + str(mb) + " Mb :)")
+    except ValueError:
+        print ("When you type bytes, only integers are allowed")
         exit(1)
-    mb = (int(b) / 1048576)
-
-    print("\n" + str(b) + " bytes are " + str(mb) + " Mb :)")
 
 def b2gb():
-    b = input("Express a number in Bytes: ")
-    if not b.isdigit():
-        print ("Only integers are allowed")
+    try:
+        b = input("Express a number in Bytes: ")
+        gb = (int(b) / 1024 ** 3)
+    except ValueError:    
+        print ("When you type bytes, only integers are allowed")
         exit(1)
-    gb = (int(b) / 1073741824)
 
     print("\n" + str(b) + " bytes are " + str(gb) + " Gb :)")
 
 def b2tb():
-    b = input("Express a number in Bytes: ")
-    if not b.isdigit():
-        print ("Only integers are allowed")
+    try:
+        b = input("Express a number in Bytes: ")
+        tb = (int(b) / 1024 ** 4)
+    except ValueError:    
+        print ("When you type bytes, only integers are allowed")
         exit(1)
-    tb = (int(b) / 1099511627776)
 
     print("\n" + str(b) + " bytes are " + str(tb) + " Tb :)")
 
 def kb2b():
-    b = input("Express a number in Kilobytes: ")
-    if not b.isdigit():
-        print ("Only integers are allowed")
+    try:
+        b = input("Express a number in Kilobytes: ")
+        kb = (float(b) * 1024)
+        kb = round(kb)
+        print("\n" + str(b) + " kilobytes are " + str(kb) + " bytes :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    kb = (int(b) * 1024)
-
-    print("\n" + str(b) + " kilobytes are " + str(kb) + " bytes :)")
 
 def kb2mb():
-    kb = input("Express a number in Kilobytes: ")
-    if not kb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        kb = input("Express a number in Kilobytes: ")
+        mb = (float(kb) / 1024)
+        print("\n" + str(kb) + " kilobytes are " + str(mb) + " Mb :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    mb = (int(kb) / 1024)
-
-    print("\n" + str(kb) + " kilobytes are " + str(mb) + " Mb :)")
 
 def kb2gb():
-    kb = input("Express a number in Kilobytes: ")
-    if not kb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        kb = input("Express a number in Kilobytes: ")
+        gb = (float(kb) / 1024 ** 2)
+        print("\n" + str(kb) + " kilobytes are " + str(gb) + " Gb :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    gb = (int(kb) / 1048576)
-
-    print("\n" + str(kb) + " kilobytes are " + str(gb) + " Gb :)")
 
 def kb2tb():
-    kb = input("Express a number in Kilobytes: ")
-    if not kb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        kb = input("Express a number in Kilobytes: ")
+        tb = (float(kb) / 1024 ** 3)
+        print("\n" + str(kb) + " kilobytes are " + str(tb) + " Tb :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    tb = (int(kb) / 1073741824)
-
-    print("\n" + str(kb) + " kilobytes are " + str(tb) + " Tb :)")
 
 def mb2kb():
-    mb = input("Express a number in megabytes: ")
-    if not mb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        mb = input("Express a number in megabytes: ")
+        kb = (float(mb) * 1024)
+        print("\n" + str(mb) + " megabytes are " + str(kb) + " Kb :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    kb = (int(mb) * 1024)
-
-    print("\n" + str(mb) + " megabytes are " + str(kb) + " Kb :)")
 
 def mb2b():
     mb = input("Express a number in megabytes: ")
-    if not mb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        b = (float(mb) * 1024 ** 2)
+        b = round(b)
+        print("\n" + str(mb) + " megabytes are " + str(b) + " bytes :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    b = (int(mb) * 1048576)
-
-    print("\n" + str(mb) + " megabytes are " + str(b) + " bytes :)")
 
 def mb2gb():
-    mb = input("Express a number in megabytes: ")
-    if not mb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        mb = input("Express a number in megabytes: ")
+        gb = (float(mb) / 1024)
+        print("\n" + str(mb) + " megabytes are " + str(gb) + " Gb :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    gb = (int(mb) / 1024)
-
-    print("\n" + str(mb) + " megabytes are " + str(gb) + " Gb :)")
 
 def mb2tb():
-    mb = input("Express a number in megabytes: ")
-    if not mb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        mb = input("Express a number in megabytes: ")
+        tb = (float(mb) / 1024 ** 2)
+        print("\n" + str(mb) + " megabytes are " + str(tb) + " Tb :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    tb = (int(mb) / 1048576)
-
-    print("\n" + str(mb) + " megabytes are " + str(tb) + " Tb :)")
 
 def gb2kb():
-    gb = input("Express a number in Gigabytes: ")
-    if not gb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        gb = input("Express a number in Gigabytes: ")
+        kb = (float(gb) * 1024 ** 2)
+        print("\n" + str(gb) + " gigabytes are " + str(kb) + " Kb :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    kb = (int(gb) * 1048576)
-
-    print("\n" + str(gb) + " gigabytes are " + str(kb) + " Kb :)")
 
 def gb2mb():
-    gb = input("Express a number in Gigabytes: ")
-    if not gb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        gb = input("Express a number in Gigabytes: ")
+        mb = (float(gb) * 1024)
+        print("\n" + str(gb) + " gigabytes are " + str(mb) + " Mb :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    mb = (int(gb) * 1024)
-
-    print("\n" + str(gb) + " gigabytes are " + str(mb) + " Mb :)")
 
 def gb2b():
-    b = input("Express a number in Gigabytes: ")
-    if not b.isdigit():
-        print ("Only integers are allowed")
+    try:
+        b = input("Express a number in Gigabytes: ")
+        gb = (float(b) * 1024 ** 3)
+        gb = round(gb)
+        print("\n" + str(b) + " gigabytes are " + str(gb) + " bytes :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    gb = (int(b) * 1073741824)
-
-    print("\n" + str(b) + " gigabytes are " + str(gb) + " bytes :)")
 
 def gb2tb():
-    gb = input("Express a number in Gigabytes: ")
-    if not gb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        gb = input("Express a number in Gigabytes: ")
+        tb = (float(gb) / 1024)
+        print("\n" + str(gb) + " gigabytes are " + str(tb) + " Tb :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    tb = (int(gb) / 1024)
-
-    print("\n" + str(gb) + " gigabytes are " + str(tb) + " Tb :)")
 
 def tb2kb():
-    tb = input("Express a number in Terabytes: ")
-    if not tb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        tb = input("Express a number in Terabytes: ")
+        kb = (float(tb) * 1024 ** 3)
+        print("\n" + str(tb) + " Terabytes are " + str(kb) + " Kb :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    kb = (int(tb) * 1073741824)
-
-    print("\n" + str(tb) + " Terabytes are " + str(kb) + " Kb :)")
 
 def tb2mb():
-    tb = input("Express a number in Terabytes: ")
-    if not tb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        tb = input("Express a number in Terabytes: ")
+        mb = (float(tb) * 1024 ** 2)
+        print("\n" + str(tb) + " terabytes are " + str(mb) + " Mb :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    mb = (int(tb) * 1048576)
-
-    print("\n" + str(tb) + " terabytes are " + str(mb) + " Mb :)")
 
 def tb2gb():
-    tb = input("Express a number in Terabytes: ")
-    if not tb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        tb = input("Express a number in Terabytes: ")
+        gb = (float(tb) * 1024)
+        print("\n" + str(tb) + " Terabytes are " + str(gb) + " Gb :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    gb = (int(tb) * 1024)
-
-    print("\n" + str(tb) + " Terabytes are " + str(gb) + " Gb :)")
 
 def tb2b():
-    tb = input("Express a number in Terabytes: ")
-    if not tb.isdigit():
-        print ("Only integers are allowed")
+    try:
+        tb = input("Express a number in Terabytes: ")
+        b = (float(tb) * 1024 ** 4)
+        b = round(b)
+        print("\n" + str(tb) + " Terabytes are " + str(b) + " bytes :)")
+    except ValueError:
+        print("Only numbers are allowed")
         exit(1)
-    b = (int(tb) * 1099511627776)
-
-    print("\n" + str(tb) + " Terabytes are " + str(b) + " bytes :)")
--- Conversion Tool/main.py
+++ Conversion Tool/main.py
@@ -1,95 +1,105 @@
 import formulas
 import sys
 
-x = None
+x = None # Source Unit
+y = None # Conversion Unit
 
-x = input("In what unit will you express your number?(B, Kb, Mb, Gb, Tb)\n")
-    
-if (x != "B" or x != "b" or x != "kb" or x != "Kb" or x != "mb" or x != "Mb" or x != "gb" or x != "Gb" or x != "tb" or x != "Tb"):
-    while (x != "B" or x != "b" or x != "kb" or x != "Kb" or x != "mb" or x != "Mb" or x != "gb" or x != "Gb" or x != "tb" or x != "Tb"):    
-        if (x == "B" or x == "b" or x == "kb" or x == "Kb" or x == "mb" or x == "Mb" or x == "gb" or x == "Gb" or x == "tb" or x == "Tb"):
-            break
-        if (x != "B" or x != "b" or x != "kb" or x != "Kb" or x != "mb" or x != "Mb" or x != "gb" or x != "Gb" or x != "tb" or x != "Tb"):
-            x = input("Type a Data Unit(B, Kb, Mb, Gb, Tb)\n")
+while (y != "b" or y != "kb" or y != "mb" or y != "gb" or y != "tb"): 
 
-y = input("To which unit do you want to convert your number?\n")
+    x = input("In what unit will you express your number?(B, Kb, Mb, Gb, Tb)\n").lower()
 
-if (y != "B" or y != "b" or y != "kb" or y != "Kb" or y != "mb" or y != "Mb" or y != "gb" or y != "Gb" or y != "tb" or y != "Tb"):
-    while (y != "B" or y != "b" or y != "kb" or y != "Kb" or y != "mb" or y != "Mb" or y != "gb" or y != "Gb" or y != "tb" or y != "Tb"):    
-        if (y == "B" or y == "b" or y == "kb" or y == "Kb" or y == "mb" or y == "Mb" or y == "gb" or y == "Gb" or y == "tb" or y == "Tb"):
-            break
-        if (y != "B" or y != "b" or y != "kb" or y != "Kb" or y != "mb" or y != "Mb" or y != "gb" or y != "Gb" or y != "tb" or y != "Tb"):
-            y = input("Type a Data Unit(B, Kb, Mb, Gb, Tb)\n")
+    y = input("To which unit do you want to convert your number?\n").lower()
 
 # vars x and y will be used to call the conversion functions in the formulas module
 
 # Below there are conditionals that will use the correct conversion function depending on your input
 
-if (x == y):
-    sys.exit("Restart the program, converting 2 same units doesn't make sense")
+    if (x == y):
+        sys.exit("Restart the program, converting 2 same units doesn't make sense")
 
-#b2units
-if (x == "b" or x == "B"): 
-    if (y == "kb" or y == "Kb"):
-        formulas.b2kb()
-if (x == "b" or x == "B"): 
-    if (y == "mb" or y == "Mb"):
-        formulas.b2mb()
-if (x == "b" or x == "B"):
-    if (y == "Gb" or y == "gb"):
-        formulas.b2gb()
-if (x == "b" or x == "B"):
-    if (y == "Tb" or y == "tb"):
-        formulas.b2tb()
-#kb2units
-if (x == "kb" or x == "Kb"): 
-    if (y == "B" or y == "b"):
-        formulas.kb2b()
-if (x == "kb" or x == "Kb"): 
-    if (y == "mb" or y == "Mb"):
-        formulas.kb2mb()
-if (x == "kb" or x == "Kb"):
-    if (y == "Gb" or y == "gb"):
-        formulas.kb2gb()
-if (x == "kb" or x == "Kb"):
-    if (y == "Tb" or y == "tb"):
-        formulas.kb2tb()
-#mb2units
-if (x == "mb" or x == "Mb"): 
-    if (y == "kb" or y == "Kb"):
-        formulas.mb2kb()
-if (x == "mb" or x == "Mb"): 
-    if (y == "b" or y == "B"):
-        formulas.mb2b()
-if (x == "mb" or x == "Mb"):
-    if (y == "Gb" or y == "gb"):
-        formulas.mb2gb()
-if (x == "mb" or x == "Mb"):
-    if (y == "Tb" or y == "tb"):
-        formulas.mb2tb()
-#gb2units
-if (x == "gb" or x == "Gb"): 
-    if (y == "kb" or y == "Kb"):
-        formulas.gb2kb()
-if (x == "gb" or x == "Gb"): 
-    if (y == "mb" or y == "Mb"):
-        formulas.gb2mb()
-if (x == "gb" or x == "Gb"):
-    if (y == "b" or y == "B"):
-        formulas.gb2b()
-if (x == "gb" or x == "Gb"):
-    if (y == "Tb" or y == "tb"):
-        formulas.gb2tb()
-#tb2units
-if (x == "tb" or x == "Tb"): 
-    if (y == "kb" or y == "Kb"):
-        formulas.tb2kb()
-if (x == "tb" or x == "Tb"): 
-    if (y == "mb" or y == "Mb"):
-        formulas.tb2mb()
-if (x == "tb" or x == "Tb"):
-    if (y == "Gb" or y == "gb"):
-        formulas.tb2gb()
-if (x == "tb" or x == "Tb"):
-    if (y == "b" or y == "B"):
-        formulas.tb2b()
+# b2units
+    if (x == "b"): 
+        if (y == "kb"):
+            formulas.b2kb()
+            exit(0)
+    if (x == "b"): 
+        if (y == "mb"):
+            formulas.b2mb()
+            exit(0)
+    if (x == "b"):
+        if (y == "gb"):
+            formulas.b2gb()
+            exit(0)
+    if (x == "b"):
+        if (y == "tb"):
+            formulas.b2tb()
+            exit(0)
+# kb2units
+    if (x == "kb"): 
+        if (y == "b"):
+            formulas.kb2b()
+            exit(0)
+    if (x == "kb"): 
+        if (y == "mb"):
+            formulas.kb2mb()
+            exit(0)
+    if (x == "kb"):
+        if (y == "gb"):
+            formulas.kb2gb()
+            exit(0)
+    if (x == "kb"):
+        if (y == "tb"):
+            formulas.kb2tb()
+            exit(0)
+# mb2units
+    if (x == "mb"): 
+        if (y == "kb"):
+            formulas.mb2kb()
+            exit(0)
+    if (x == "mb"): 
+        if (y == "b"):
+            formulas.mb2b()
+            exit(0)
+    if (x == "mb"):
+        if (y == "gb"):
+            formulas.mb2gb()
+            exit(0)
+    if (x == "mb"):
+        if (y == "tb"):
+            formulas.mb2tb()
+            exit(0)
+# gb2units
+    if (x == "gb"): 
+        if (y == "kb"):
+            formulas.gb2kb()
+            exit(0)
+    if (x == "gb"): 
+        if (y == "mb"):
+            formulas.gb2mb()
+            exit(0)
+    if (x == "gb"):
+        if (y == "b"):
+            formulas.gb2b()
+            exit(0)
+    if (x == "gb"):
+        if (y == "tb"):
+            formulas.gb2tb()
+            exit(0)
+# tb2units
+    if (x == "tb"): 
+        if (y == "kb"):
+            formulas.tb2kb()
+            exit(0)
+    if (x == "tb"): 
+        if (y == "mb"):
+            formulas.tb2mb()
+            exit(0)
+    if (x == "tb"):
+        if (y == "gb"):
+            formulas.tb2gb()
+            exit(0)
+    if (x == "tb"):
+        if (y == "b"):
+            formulas.tb2b()
+            exit(0)
+    print("Enter a valid input for source and/or conversion unit\n")   
